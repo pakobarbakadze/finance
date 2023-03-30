2b016c6eddfc374150a46f51eeb689970db1d9ce
pakobarbakadze <pakobarbakadze@gmail.com>
2023-03-30 12:03:51 +0400

    Dashboard, Predictions.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,32 @@
-import { useState } from 'react'
-import './App.css'
+import { Box } from "@mui/material";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+import { useMemo } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { themeSettings } from "./theme";
+
+import Navbar from "./components/Navbar";
+import DashboardPage from "./pages/DashboardPage";
+import PredictionsPage from "./pages/PredictionsPage";
 
 function App() {
-
+  const theme = useMemo(() => createTheme(themeSettings), []);
   return (
-    <div className="App">
+    <div className="app">
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Box width="100%" height="100%" padding="1rem 2rem 4rem 2rem">
+            <Navbar />
+            <Routes>
+              <Route path="/" element={<DashboardPage />} />
+              <Route path="/predictions" element={<PredictionsPage />} />
+            </Routes>
+          </Box>
+        </ThemeProvider>
+      </BrowserRouter>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
